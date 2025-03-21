2d153dc4ac1df906ee6940028275f38f7cf7a1d9
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-03-21 08:41:56 +0100

    added auth jwt to createupdate and delete a review

--- routes/review.route.js
+++ routes/review.route.js
@@ -3,9 +3,9 @@
 const ReviewController = require('../controllers/review.controller');
 
 module.exports = [
-    { method: 'POST',   path: '/reviews',               handler: ReviewController.createReview }, //create review
-    { method: 'GET',    path: '/reviews/book/{bookId}', handler: ReviewController.getReviewsByBook }, //get reviews for a book
-    { method: 'GET',    path: '/reviews/user',          handler: ReviewController.getReviewsByUser, options: {auth: 'jwt'} }, //get reviews for a user
-    { method: 'PUT',    path: '/reviews/{reviewId}',    handler: ReviewController.updateReview }, //Update a review
-    { method: 'DELETE', path: '/reviews/{reviewId}',    handler: ReviewController.deleteReview } //delete a review
+    { method: 'POST',   path: '/reviews',               handler: ReviewController.createReview, options: { auth: 'jwt' } },//create review
+    { method: 'GET',    path: '/reviews/book/{bookId}', handler: ReviewController.getReviewsByBook }, //public, no need for jwt
+    { method: 'GET',    path: '/reviews/user',          handler: ReviewController.getReviewsByUser, options: { auth: 'jwt' } }, //user-specific reviews
+    { method: 'PUT',    path: '/reviews/{reviewId}',    handler: ReviewController.updateReview, options: { auth: 'jwt' } }, //update review
+    { method: 'DELETE', path: '/reviews/{reviewId}',    handler: ReviewController.deleteReview, options: { auth: 'jwt' } }  //Delete review
 ];
